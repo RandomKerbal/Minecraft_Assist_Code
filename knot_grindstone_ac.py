ca5399519887495927dd559ec8bb48b243201628
ChaiZY <chaiZY0729@gmail.com>
2025-05-18 19:28:55 -0400

    2

--- knot_grindstone_ac.py
+++ knot_grindstone_ac.py
@@ -1,4 +1,6 @@
 from knot_fence_ac import adjs
+states = ('standing', 'side', 'hanging')
+states2 = ('0', '1', '2', '3',)
 
 if __name__ == "__main__":
     for adj in adjs:
@@ -10,3 +12,18 @@ if __name__ == "__main__":
         output += ' 0 0"'
         print(output)
     print()
+
+    for state in states:
+        for state2 in states2:
+            if not ((state == 'standing' and state2 in ('0', '2', '3',)) or (state == 'hanging' and state2 in ('2', '3',))):
+                output = ''
+                output += r'"/execute at @s if block ~~~ grindstone [\"attachment\"=\"'
+                output += state
+                output += r'\", \"direction\"='
+                output += state2
+                output += '] run event entity @s is_'
+                output += state
+                output += state2
+                output += '",'
+                print(output)
+        print()
---
+++ knot_grindstone_events.py
@@ -0,0 +1,13 @@
+from knot_grindstone_ac import states, states2
+
+if __name__ == "__main__":
+    for i, state in enumerate(states):
+        for state2 in states2:
+            if not ((state == 'standing' and state2 in ('0', '2', '3',)) or (state == 'hanging' and state2 in ('2', '3',))):
+                output = '"is_'
+                output += state + state2
+                output += '":{"add":{"component_groups":["is_'
+                output += state + state2
+                output += '"]}},'
+                print(output)
+        print()
